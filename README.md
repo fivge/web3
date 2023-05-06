6c68f92ca854e37fe9bdb616d3793481fa125cc9
fivge <luanxingtong@gmail.com>
2023-05-06 17:35:53 +0800

    feat: use sepolia testnet

--- README.md
+++ README.md
@@ -4,9 +4,14 @@
 
 ##### links
 
-+ https://zhuanlan.zhihu.com/p/414635679
-+ https://www.preethikasireddy.com/post/the-architecture-of-a-web-3-0-application?ref=tokendaily
-+ https://github.com/OffcierCia/DeFi-Developer-Road-Map
-+ https://learnku.com/articles/72903
-+ https://github.com/Dapp-Learning-DAO/Dapp-Learning/blob/main/README-CN.md
-+ https://github.com/FrancescoXX/free-Web3-resources
+- https://zhuanlan.zhihu.com/p/414635679
+- https://www.preethikasireddy.com/post/the-architecture-of-a-web-3-0-application?ref=tokendaily
+- https://github.com/OffcierCia/DeFi-Developer-Road-Map
+- https://learnku.com/articles/72903
+- https://github.com/Dapp-Learning-DAO/Dapp-Learning/blob/main/README-CN.md
+- https://github.com/FrancescoXX/free-Web3-resources
+
+##### chain
+
+- https://faucets.chain.link/
+- https://testnet.help/en/ethfaucet/ropsten
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let Web3 = require("web3");
-let solc = require("solc");
-let fs = require("node:fs");
-// let fs = require("fs");
+const Web3 = require("web3");
+const solc = require("solc");
+const fs = require("node:fs");
 // Get privatekey from environment
 require("dotenv").config();
 const privatekey = process.env.PRIVATE_KEY;
+
+// const web3Api = "https://goerli.infura.io/v3/" + process.env.INFURA_ID;
+const web3Api = "https://sepolia.infura.io/v3/" + process.env.INFURA_ID;
 
 // Load contract
 const source = fs.readFileSync("Incrementer.sol", "utf8");
@@ -34,7 +36,7 @@ const bytecode = contractFile.evm.bytecode.object;
 const abi = contractFile.abi;
 
 // Create web3 with goerli provider，you can change goerli to other testnet
-const web3 = new Web3("https://goerli.infura.io/v3/" + process.env.INFURA_ID);
+const web3 = new Web3(web3Api);
 
 // Create account from privatekey
 const account = web3.eth.accounts.privateKeyToAccount(privatekey);
@@ -60,10 +62,12 @@ const Deploy = async () => {
   const deployTransaction = await web3.eth.accounts.signTransaction(
     {
       data: deployTx.encodeABI(),
-      gas: 8000000,
+      gas: 0xf4240,
     },
     account_from.privateKey
   );
+
+  console.log("deployTransaction", deployTransaction);
 
   const deployReceipt = await web3.eth.sendSignedTransaction(
     deployTransaction.rawTransaction
@@ -71,6 +75,7 @@ const Deploy = async () => {
 
   // Your deployed contrac can be viewed at: https://goerli.etherscan.io/address/${deployReceipt.contractAddress}
   // You can change goerli in above url to your selected testnet.
+  // sepolia
   console.log(`Contract deployed at address: ${deployReceipt.contractAddress}`);
 };
 
